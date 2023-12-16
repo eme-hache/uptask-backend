17b0a22e881277255ef9bda67c163dfbd3f3370e
Eme Hache <luisalfonsodsg@outlook.com>
2023-12-16 12:47:51 -0500

    chore: update mongoose conector

--- config/db.js
+++ config/db.js
@@ -2,10 +2,8 @@ import mongoose from 'mongoose'
 
 const connect = async () => {
     try {
-        const connection = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
+        console.log(process.env.MONGO_URI)
+        const connection = await mongoose.connect(process.env.MONGO_URI)
 
         const url = `${connection.connection.host}: ${connection.connection.port}`
         console.log(`Connected to ${url}`)
